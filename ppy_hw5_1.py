2cc51bd94f080f3622fd96e0e12b6ea291a13deb
hussalen <s26136@pjwstk.edu.pl>
2024-04-14 22:17:17 +0200

    init

---
+++ ppy_hw5_1.py
@@ -0,0 +1,9 @@
+
+
+class ShoppingList:
+    pass
+# Load Shopping List From File
+# Add Product to the list (params: name of prod, quantity)
+# Remove product from list (param: name)
+# Display shopping list (each product and its quantity, separated by \t)
+# Save shopping list to file (just use above and save to txt lol)
